03c7e93a6b4caeec561bacd81d177cee5417d2b7
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-03-01 12:42:57 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

---
+++ Workings/Grid.py
@@ -0,0 +1,22 @@
+#creates the grid with initial values of False
+class Grid: 
+
+    #input requirements:
+    # num s.t. int > 0
+    
+    #input: num=int
+    #output: .rows = int; .columns = int; .grid = list of lists
+    def __init__(self, num):
+        self.rows = int(num)
+        self.columns = int(num)
+        self.grid = self.generateGrid()
+
+    #input: n/a
+    #output: list of lists
+    def generateGrid(self):
+        gridMult = self.rows + 1
+        theGrid = [ [False]*gridMult for s in range(0, gridMult, 1) ]
+        return theGrid
+    
+aGrid = Grid(0).grid
+print(aGrid)
