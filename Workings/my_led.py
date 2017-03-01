2e720984c56c2f632ba2c1e7641ab3eeefa07302
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-03-01 20:12:52 +0000

    Signed-off-by: Otto Hermann <otto.hermann@ucdconnect.ie>

---
+++ Workings/my_led.py
@@ -0,0 +1,38 @@
+#imports
+import argparse
+import my_classes
+import sys
+
+#argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("source", help="a valid url")
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-i", "--input", action="store_true")
+
+args = parser.parse_args()
+
+#link
+theSource = CleanLink(args.source).clean
+if not my_classes.validLink(theSource):
+    print("That was not a valid url.")    
+
+#size
+theSource = urllib.request.urlopen(theSource)
+theSize = 0
+with theSource as theSrc:
+    #clean
+    theSrc = theSrc.readline().decode('utf-8')
+    theSrc = sRemoveLeftWhiteSpace(theSrc)
+    #checks
+    if not sCheckSize(theSrc):
+        print("There was more than one number input for size")
+        sys.exit()
+    if not sCheckInt(theSrc):
+        print("Whatever was entered for size is not Python-integer friendly.")
+        sys.exit()
+    if not sCheckRange(theSrc):
+        print("The integer entered for the size is not within the appropriate range [0, 10^9]")
+        sys.exit()
+    theSize = int(theSrc)
+
