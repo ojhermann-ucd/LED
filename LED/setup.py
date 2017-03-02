caf77747495debd59474de21f373c1aba6ab59b8
ojhermann-ucd <otto.hermann@ucdconnect.ie>
2017-03-02 17:31:30 +0000

    Adding setup.py

---
+++ LED/setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "comp30670Assignment3 by Otto Hermann aka 16203034",
+    description = "comp30670 Assignment 3, Spring 2017",
+    author = "Otto Hermann",
+    author_email = "otto.hermann@ucdconnect.ie",
+    url="www.haha-no-i-do-not-have-one.org",
+    packages = ['Workings', 'Tests'],
+    )
